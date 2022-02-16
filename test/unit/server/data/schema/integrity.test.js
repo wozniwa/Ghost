959869b8365a2140adfe70d38c0cefbce9aacb4d
Matt Hanley <git@matthanley.co.uk>
2022-02-16 14:34:08 +0000

    Updated fixtures to add post feature_image
    - Added feature_image to initial _Coming soon_ post in fixtures
    - Removed preceding slash from secondary nav link to Portal so it opens on the current page
    - Removed GHOST_URL from mobiledoc to open Portal on the current page

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -36,8 +36,8 @@ const validateRouteSettings = require('../../../../../core/server/services/route
 describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = 'c8442427463a0bf25ecca871b01016dc';
-    const currentFixturesHash = 'beb040c0376a492c2a44767fdd825a3e';
-    const currentSettingsHash = '437d4c6da8759f5c35f11f811b86e5bc';
+    const currentFixturesHash = '9a47c3f855d96737181e8dda56788145';
+    const currentSettingsHash = '71fa38d0c805c18ceebe0fda80886230';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
